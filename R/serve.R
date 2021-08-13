9349b69029411d2e2a149b56d49857e5de2df862
Kai Aragaki <adamaragaki@gmail.com>
2021-08-13 12:09:54 -0400

    Serve meta along with rest of layers

--- R/serve.R
+++ R/serve.R
@@ -10,9 +10,12 @@ setGeneric("serve", function(x) standardGeneric("serve"))
 
 #' @describeIn Plate method to convert a plate object into a tidy tibble
 setMethod("serve", "Plate", function(x) {
+
   x@layers |>
     lapply(as.matrix) |>
     lapply(as.vector) |>
     purrr::reduce(dplyr::bind_cols) |>
-    stats::setNames(names(x@layers))
+    stats::setNames(names(x@layers)) |>
+    dplyr::bind_cols(x@meta)
+
 })
