b2100169098c73053b5ec9ffa09334c7b0dcc7b0
Kai Aragaki <adamaragaki@gmail.com>
2022-01-12 17:06:22 -0500

    Add serve roxygen header

--- R/gp_serve.R
+++ R/gp_serve.R
@@ -1,3 +1,11 @@
+#' Extract useful, tidy data from a gp object
+#'
+#' @param gp a gp
+#'
+#' @return a `tibble`, with `.row` and `.col`, as well as any created section names and data values.
+#' @export
+#'
+#' @examples
 gp_serve <- function(gp) {
   gp$well_data |>
     dplyr::select(-dplyr::starts_with("."), .row, .col) |>
