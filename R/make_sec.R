3ff2c422778385eb5f4f84ccb05a988321c7fd31
KaiAragaki <adamaragaki@gmail.com>
2022-01-25 15:38:00 -0500

    remove (probably) redundant code

--- R/make_sec.R
+++ R/make_sec.R
@@ -1,27 +1,5 @@
 make_sec <- function(gp, flow, wrap) {
 
-  if (flow == "row") {
-    gp$well_data <- dplyr::arrange(gp$well_data, .sec_par, .row_sec_par_rel, .col_sec_par_rel, .row_sec, .col_sec)
-  } else {
-    gp$well_data <- dplyr::arrange(gp$well_data, .sec_par, .col_sec_par_rel, .row_sec_par_rel, .col_sec, .row_sec)
-  }
-
-  gp$well_data <- gp$well_data |>
-    dplyr::group_by(.sec_par, .row_sec_rel, .col_sec_rel) |>
-    dplyr::mutate(.sec = 1:dplyr::n())
-
-  if (flow == "row") {
-    gp$well_data <- gp$well_data |>
-      dplyr::mutate(.sec = ifelse(.col_sec_rel == 1, .sec, NA_integer_))
-  } else {
-    gp$well_data <- gp$well_data |>
-      dplyr::mutate(.sec = ifelse(.row_sec_rel == 1, .sec, NA_integer_))
-  }
-
-
-  gp$well_data <- dplyr::ungroup(gp$well_data)
-
-
   fake_sec <- tidyr::expand_grid(.row_sec_par = gp$row_coord_map$.row_sec_par, .col_sec_par = gp$col_coord_map$.col_sec_par)
 
   fake_sec_full <- fake_sec |> dplyr::left_join(gp$row_coord_map) |> dplyr::left_join(gp$col_coord_map) |>
