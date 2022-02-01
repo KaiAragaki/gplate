98d6369a942d05f3e18c07d60b03ad406ccf99e9
KaiAragaki <adamaragaki@gmail.com>
2022-02-01 12:21:51 -0500

    continue working on new margin strat

--- R/coord_map.R
+++ R/coord_map.R
@@ -1,59 +1,43 @@
 coord_map <- function(gp, type = c("row", "col"), margin) {
 
-  # ?new margin strat?
-  # use nrow, then just pop in margins before and after as NAs
-  # If ncol = c(3, 2, 1)
-  # and margin = 1
-  # NA 3 3 3 NA NA 2 2 NA NA 1 NA NA 3 NA
-  #
-  # Something like
-  # c(rbind(margin))
-
   type <- rlang::arg_match(type)
 
-  dim_sec_par <- ifelse(type == "row", gp$nrow_sec_par, gp$ncol_sec_par)
-  dim_sec <- if(type == "row") gp$nrow_sec else gp$ncol_sec
-  backwards_corners <- if(type == "row") c("bl", "br") else c("tr", "br")
+  if (type == "row") {
+    dim_sec_par <- gp$nrow_sec_par
+    dim_sec <- gp$nrow_sec
+    backwards_corners <- c("bl", "br")
+    margin_head_size <- margin$bottom
+    margin_tail_size <- margin$top
+    sec_par <- seq_len(gp$nrow_sec_par)
+  } else {
+    dim_sec_par <- gp$ncol_sec_par
+    dim_sec <- gp$ncol_sec
+    backwards_corners <- c("tr", "br")
+    margin_head_size <- margin$right
+    margin_tail_size <- margin$left
+    sec_par <- seq_len(gp$ncol_sec_par)
+  }
+
   is_backwards <- gp$start_corner %in% backwards_corners
-  margin_head_size <- if (type == "row") margin$bottom else margin$right
-  margin_tail_size <- if (type == "row") margin$top else margin$left
 
-  ## Now where does this all fit in with everything?
-  # purrr::pmap(list(mh = margin_head_size, ds = dim_sec, mt = margin_tail_size), ~ tibble::tibble(n = c(..1, ..2, ..3), is_margin = c(T, F, T))) |>
-  #   purrr::reduce(dplyr::bind_rows) |>
-  #   dplyr::mutate(my_data = purrr::map2(n, is_margin, ~ tibble::tibble(seq = seq_len(.x), my_margin = rep(.y, .x)))) |>
-  #   tidyr::unnest(my_data)
-
-  dim_sec_mar <- dim_sec + margin_head_size + margin_tail_size
-
-  margin_head <- (dim_sec_mar + 1 - margin_head_size):(dim_sec_mar + 1)
-  margin_tail <- 0:margin_tail_size
-
-  sec_par <- if (type == "row") gp$nrow_sec_par else gp$ncol_sec_par
-  sec_par <- seq_len(sec_par)
+  tt <-
+    purrr::pmap(list(mh = margin_head_size, ds = dim_sec, mt = margin_tail_size), ~ tibble::tibble(n = c(..1, ..2, ..3), is_margin = c(T, F, T))) |> # TODO Should add section index at this point
+    purrr::reduce(dplyr::bind_rows) |>
+    dplyr::mutate(my_data = purrr::map2(n, is_margin, ~ tibble::tibble(seq = seq_len(.x), my_margin = rep(.y, .x))),
+                  my_data = purrr::map(my_data, \(x){if(is_backwards) rev(x$seq) else x$seq})) |> # TODO Should ADD a rel col instead of just changing it to one
+    tidyr::unnest(my_data) |>
+    dplyr::mutate(my_data = if_else(is_margin, NA_integer_, my_data)) |>
+    dplyr::select(-n)
 
   if (gp$start_corner_par %in% backwards_corners) sec_par_rel <- rev(sec_par) else sec_par_rel <- sec_par
 
-  seq2 <- Vectorize(seq.default, vectorize.args = "from")
+  tt <- replicate((nrow(coord_map) %/% nrow(tt)) + 1, tt, simplify = FALSE) |>
+    dplyr::bind_rows() |>
+    dplyr::slice_head(n = nrow(coord_map))
 
   coord_map <-
     dplyr::tibble(sec_par, sec_par_rel) |>
-    dplyr::arrange(sec_par) |>
-    dplyr::mutate(sec_rel = rep_len(unlist(seq2(dim_sec + margin_head_size + margin_tail_size)), dim_sec_par))
-
-  if (is_backwards) {
-    coord_map <- dplyr::mutate(coord_map, sec_rel = rev(sec_rel))
-  }
-
-  coord_map <- coord_map |>
-    dplyr::rowwise() |>
-    dplyr::mutate(is_margin = sec_rel %in% c(margin_head, margin_tail),
-                  sec_rel = ifelse(is_margin, NA_integer_, sec_rel)) |>
-    dplyr::ungroup() |>
-    dplyr::mutate(sec_rel = sec_rel - min(sec_rel, na.rm = TRUE) + 1) |> # Set lowest value to 1 after margin removal
-    dplyr::rowwise() |>
-    dplyr::mutate(sec = ifelse(is_backwards, -sec_rel + dim_sec + 1, sec_rel)) |>
-    dplyr::ungroup() |>
+    dplyr::bind_cols(tt) |>
     dplyr::arrange(sec_par) |>
     dplyr::rename_with(~ paste0(".", type, "_", .x))
 
