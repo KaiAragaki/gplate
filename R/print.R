0db974383e5921dba098d1e8d07beb0e3ffb2050
Kai Aragaki <adamaragaki@gmail.com>
2022-04-12 20:23:58 -0400

    continue print work

--- R/print.R
+++ R/print.R
@@ -6,16 +6,25 @@
 #' @export
 print.gp <- function(x, ...) {
 
-  l_mar <- rep(" ", nchar(x$nrow) + 1)
+  if (x$nrow_sec > 50 | x$ncol_sec + 5 > cli::console_width()) {
+    cli::cli_text(cli::col_silver("Section dimensions: "),  "{x$nrow_sec} x {x$ncol_sec}")
+  } else {
+    l_mar <- rep(" ", nchar(x$nrow_sec_mar) + 1)
 
-  cat(c("\n", l_mar, " ", rep(" ", x$ncol_sec %/% 2),
-        x$ncol_sec,
-        "\n"), sep = "")
+    cat(cli::col_silver(c("\n", l_mar, "  ", rep("  ", x$ncol_sec_mar %/% 2),
+          cli::col_none(x$ncol_sec_mar),
+          "\n")), sep = "")
 
-  cat(c(l_mar, " ", rep("_", x$ncol_sec)), sep = "")
-  cat(c("\n", rep(c(l_mar, "|", rep("o", x$ncol_sec), "\n"), x$nrow_sec %/% 2)), sep = "")
-  cat(c(x$nrow, c(" |", rep("o", x$ncol_sec), "\n")), sep = "")
-  cat(c(rep(c(l_mar, "|", rep("o", x$ncol_sec), "\n"), x$nrow_sec - x$nrow_sec %/% 2 - 1), "\n"), sep = "")
+    o <- paste0(" ", cli::symbol$circle)
 
-  cli::cli_text("Start corner: {x$start_corner}")
+    cat(cli::col_silver(c(l_mar, " ", rep("__", x$ncol_sec_mar))), sep = "")
+    cat(cli::col_silver(c("\n", rep(c(l_mar, "|", rep(o, x$ncol_sec_mar), "\n"), x$nrow_sec_mar %/% 2))), sep = "")
+    cat(cli::col_silver(c(cli::col_none(x$nrow_sec_mar), c(" |", rep(o, x$ncol_sec_mar), "\n"))), sep = "")
+    cat(cli::col_silver(c(rep(c(l_mar, "|", rep(o, x$ncol_sec_mar), "\n"), x$nrow_sec_mar - x$nrow_sec_mar %/% 2 - 1), "\n")), sep = "")
+  }
+
+
+
+  cli::cli_text(cli::col_silver("Start corner: "), "{x$start_corner}")
+  cli::cli_text(cli::col_silver("Plate dimensions: "), "{x$nrow} x {x$ncol}")
 }
